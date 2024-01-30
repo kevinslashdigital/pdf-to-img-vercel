b8924939425e5347cb94d9d37b80f5d6e53fcf1f
kevinslashdigital <36614901+kevinslashdigital@users.noreply.github.com>
2024-01-30 18:06:15 +0700

    Update polyfill.ts

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -1,7 +1,7 @@
 // @ts-expect-error pdfjs references window.xxx, which fails if the jest testEnvironment is node
-global.window ||= global;
+// global.window ||= global;
 
 // 20ms because that's what pdfjs used to do: https://github.com/mozilla/pdf.js/pull/9138/files
-global.window.requestAnimationFrame = (callback) =>
-  global.window.setTimeout(callback, 20);
-global.window.cancelAnimationFrame = (id) => global.window.clearTimeout(id);
+// global.window.requestAnimationFrame = (callback) =>
+//  global.window.setTimeout(callback, 20);
+// global.window.cancelAnimationFrame = (id) => global.window.clearTimeout(id);
